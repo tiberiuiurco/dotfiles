bd8c7c55f0744713fea713c54f9e05135aca0bea
Tibi <tiurco@amberstudio.com>
2022-08-06 15:55:04 +0300

    Small add

--- install.sh
+++ install.sh
@@ -2,10 +2,20 @@
 
 # This script will install all required packages and dependencies to build
 sudo pacman -S alacritty starship neovim tmux fish nitrogen picom zathura ranger nm-applet
+
+# Music
 sudo pacman -S cmus # music
+
+# Pass
+yay -S pam-gnupg 
+sudo pacman -S pass
+
+# Useful utils
 sudo pacman -S exa fzf ripgrep fd tokei procs
 sudo pacman -S eva # Calculator Replacement
 sudo pacman -S dust # Disk Space Utility
+
+
 # Try zoxide and decide whether it's worth adding
 # yay -S ytop # HTOP BUT WAAAY BETTER
 
